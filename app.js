dd2320e546909cf654e58287d8ea7874e4090427
girma-tesfaye <girmatesfaye80@gmail.com>
2023-09-14 00:37:00 -0700

    Progress

--- app.js
+++ app.js
@@ -8,21 +8,25 @@ const app = Vue.createApp({
                     name: "Girma.T",
                     tittle: "Sr. Full-stack developer",
                     experience: 5,
+                    isSenior: true
                 },{
                     id: 2,
                     name: "John Doe",
                     tittle: "CEO",
                     experience: 4,
+                    isSenior: true
                 },{
                     id: 3,
                     name: "Jane Doe",
                     tittle: "Sr. Devops",
                     experience: 2,
+                    isSenior: false
                 },{
                     id: 4,
                     name: "Alex Doe",
                     tittle: "Manager",
                     experience: 1,
+                    isSenior: false
                 }
             ]
     }},
@@ -36,7 +40,14 @@ const app = Vue.createApp({
             } else {
                 this.count = 1
             }
-            
+        },
+        handleClass (arr) {
+            arr.isSenior = !arr.isSenior
+        }
+    },
+    computed: {
+        filterDevs () {
+            return this.array.filter((arr) => arr.isSenior) 
         }
     }
 })
